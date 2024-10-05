32d8e6e16cc538c966b99c48c8a94fe2306d077c
smartsusee <susee37432@gmail.com>
2024-10-05 17:26:17 +0530

    backend

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require("express");
+
+const app = express();
+app.use(express.json());
+
+const mongoose = require("mongoose");
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/userData")
+
+  .then(() => {
+    console.log("db connected");
+  })
+
+  .catch(() => {
+    console.log("db i not connected");
+  });
+
+const dataschema = mongoose.Schema({
+  name: String,
+
+  email: String,
+
+  phone: String,
+});
+
+const Item = mongoose.model("Items", dataschema);
+
+app.get("/getData", async (req, res) => {
+  let data = await Item.find({});
+  res.json(data);
+});
+
+app.post("/create", async (req, res) => {
+  const data = new Item({
+    ...req.body,
+  });
+  const datas = await data.save();
+  res.json(datas);
+});
+
+app.listen(4000, () => {
+  console.log("server is running port:4000");
+});
